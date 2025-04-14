277c083ef1cfc1614322232c617bc3d874d1ebc3
Dhanashree0076 <dhanshrigadhave3@gmail.com>
2025-04-14 04:33:45 -0700

    we learn javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let newTitle = "welcome to easy code"
+
+document.title = newTitle
+
+
+let myheading = "<h1>learn javascript with easycode </h1>"
+let subheading  = "<p> we have task to complete javascript in 30 days and these is  day 7 </p>"
+
+document.writeln{myheading}
