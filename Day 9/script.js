2158d86636b954de86d120e8ed04298f9660536c
Dhanashree0076 <dhanshrigadhave3@gmail.com>
2025-04-18 08:36:28 -0700

    condition statment

---
+++ Day 9/script.js
@@ -0,0 +1,9 @@
+// let easyCodeStudents = prompt ("enter your name ? ")
+
+// if(easyCodeStudents == "dhanashree"){
+//     document.writeln("<h1>you are easy code students</h1>")
+// }
+// if(easyCodeStudents == "shreya"){
+//        document.writeln("<h1>you are easy code student</h1>")
+// }
+
