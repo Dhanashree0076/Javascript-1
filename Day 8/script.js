cbe5ab7ae7fcc7c773170ef3e9b7c7b9f5b3fe15
Dhanashree0076 <dhanshrigadhave3@gmail.com>
2025-04-19 00:11:04 -0700

    learn with javascript

---
+++ Day 8/script.js
@@ -0,0 +1,39 @@
+ // alert("you are your network !");
+
+ // let result = confirm("are you sure ?");
+
+// console.log(result)
+
+// let  result = prompt("enter your name");
+
+// document.writeln("<h1>hello" + result + "welcome in our website</h1>" )
+
+
+
+// let ope1 = prompt("enter first value:")
+// let ope2 = prompt("enter second value:")
+
+// console.log(typeof ope1)
+// console.log(typeof ope2)
+
+// let a ="hello"
+// let b ="thank you"
+
+// console.log(a + b)
+
+
+
+
+let ope2 + prompt("enter first value: ")
+let ope2 = prompt("enter second value : ")
+
+let numberOPE1 = parseInt(ope1)
+let numberOPE2 = parseInt(ope2)
+
+console.log("addition : " + (numberOPE1 + numberOPE2))
+conletsole.log("subtraction : " + (numberOPE1  - numberOPE1))   
+console.log("multiplication : " + (numberOPE1 * numberOPE2))
+console.log("division : " + (numberOPE1 / numberOPE2))
+console.log("modulus : " +(numberOPE1 % numberOPE2))
+
+
