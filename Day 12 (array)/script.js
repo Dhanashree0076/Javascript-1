e5f01d759e3f6466f15d956df46488e75672eace
Dhanashree0076 <dhanshrigadhave3@gmail.com>
2025-05-19 03:19:29 -0700

    array of the javascript

---
+++ Day 12 (array)/script.js
@@ -0,0 +1,30 @@
+//   let  cities = ["pune","satara","mumbai","delhi"];
+
+//   console.log(cities.reverse());
+
+//   console.log(cities.sort())
+
+//   console.log(cities.includes("pune"))
+
+
+// let flowers = ["rose","jasmine","mogara","hibiscus"];
+
+// console.log(flowers.reverse("rose"));
+
+// console.log(flowers.sort())
+
+// console.log(flowers.includes("rose"))
+
+// console.log(flowers.indexOf("rose"))
+
+
+let girls = ["dhanashree","shreya","akanksha","shruti","nikita","prajkta"];
+
+console.log(girls.reverse("shreya"));
+
+console.log(girls.includes("dhanashree"))
+
+console.log(girls.indexOf("akanksha"))
+
+
+
---
+++ Day 19 (DOM)/script.js
@@ -0,0 +1,13 @@
+// document.getElementById("box")
+
+// let box1 = document.getElementById("box1")
+
+// console.log(box1.innerText
+
+
+let box = document.getElementById("box2")
+
+box.innerHTML = <div>hello</div>
+
+console.log("box");
+
