9e628e26ccbba6715f3e59578c7c08652dd1ef23
Dhanashree0076 <dhanshrigadhave3@gmail.com>
2025-05-23 00:21:22 -0700

    local storage

--- Day 19 (DOM)/script.js
+++ Day 19 (DOM)/script.js
@@ -2,12 +2,63 @@
 
 // let box1 = document.getElementById("box1")
 
-// console.log(box1.innerText
+// console.log(box1.innerText)
 
 
-let box = document.getElementById("box2")
+//  let box = document.getElementById("box")
 
-box.innerHTML = <div>hello</div>
+// box.innerText = "dhanashree"
 
-console.log("box");
+// let box = document.getElementById("box1")
+
+// box.innerText ="ranika"
+
+//   let box = document.getElementById("box2")
+
+// box.innerText ="tejas"
+
+
+
+
+// document.getElementById
+
+// let box = document.getElementById("box2")
+
+// box.innerHTML = '<div>hello</div>'
+
+// console.log(box)
+
+
+// document.getElementById
+
+// let.box = document.getElementById("box2")
+
+// box.style.margin ="auto"
+
+// document.getElementById
+
+// document.getElementById ClassName
+
+// let p = document getElementById("pragragraph")
+
+// let p = document.getElementById("p")
+
+// console.log(p)
+
+// let p = document.querySelector("pragragraph")
+
+// console.lof(p)
+
+
+let btn = document.querySelector("#btn")
+
+btn.addEventListener("click",()=>){
+    btn.innerText = "unfollow"
+})
+
+
+
+
+
+
 
---
+++ Day 22/script.js
@@ -0,0 +1,24 @@
+// let btn = document.querySelector("#btn")
+
+// let gitHubUserName = document.getElementById("#gitHubinput")
+
+// btn.addEventListener("click",{}=>{
+    //    console.log(gitHubUserName.value)
+// } )
+
+
+
+// document.addEventListener("DCMContentLoaded",{}=>{
+    //   let user = localStorage.getElementById("userName")
+    //   console.log(user)
+// })
+
+
+// localStorage.setItem("name","dhanu")
+// localStorage.setItem("age",20)
+// localStorage.setItem("gender","female")
+
+
+// let age = localStorage.getItem ("age")
+// console.log(age)
+
